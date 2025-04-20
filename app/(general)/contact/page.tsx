e0acdf48c133278bbcbd20589683006d6393b6af
ImNotGabo <gabriel.duarte18@outlook.es>
2025-04-20 17:51:31 -0400

    Add ContactPage component with metadata and initial layout

---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Contact - Get in Touch',
+	description:
+		'Reach out to us for any inquiries, support, or feedback. We are here to help!',
+};
+export default function ContactPage() {
+	return (
+		<>
+			<span className='text-7xl'>Contact</span>
+		</>
+	);
+}
