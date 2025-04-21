a24bf7b647bc454040c1b301fedf37a35e9fc348
ImNotGabo <gabriel.duarte18@outlook.es>
2025-04-21 14:11:21 -0400

    Refactor Navbar component to use ActiveLink for navigation and improve layout

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,26 +1,31 @@
+import { HomeIcon } from '@primer/octicons-react';
+import Link from 'next/link';
+import ActiveLink from '../active-link/ActiveLink';
+
+const navItems = [
+	{ path: '/about', text: 'About' },
+	{ path: '/pricing', text: 'Pricing' },
+	{ path: '/contact', text: 'Contact' },
+];
+
 export default function Navbar() {
 	return (
-		<nav className='flex bg-blue-800 backdrop-opacity-30 p-2 m-2'>
-			<span>Home</span>
+		<nav className='flex bg-blue-800/30 p-2 m-2 rounded'>
+			<Link
+				href={'/'}
+				className='flex items-center justify-center'
+			>
+				<HomeIcon className='mr-2' />
+				<span>Home</span>
+			</Link>
+
 			<div className='flex flex-1'></div>
-			<a
-				className='mr-2'
-				href='/about'
-			>
-				About
-			</a>
-			<a
-				className='mr-2'
-				href='/pricing'
-			>
-				Pricing
-			</a>
-			<a
-				className='mr-2'
-				href='/contact'
-			>
-				Contact
-			</a>
+			{navItems.map((navItem, key) => (
+				<ActiveLink
+					key={key}
+					{...navItem}
+				/>
+			))}
 		</nav>
 	);
 }
